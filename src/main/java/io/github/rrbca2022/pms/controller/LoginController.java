fbf28ae5a398b17712dd21164fc4ba8bc1ab91d5
DeathGOD7 <36422471+DeathGOD7@users.noreply.github.com>
2025-12-31 18:07:09 +0545

    Test files upload

---
+++ src/main/java/io/github/rrbca2022/pms/controller/LoginController.java
@@ -0,0 +1,14 @@
+package io.github.rrbca2022.pms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/")
+	public String login() {
+		return "login.html";
+	}
+
+}
